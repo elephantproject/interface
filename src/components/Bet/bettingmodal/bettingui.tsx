a3b9a586f3a3e664606659a2014bf4b663b0fede
elephantproject <elephantproject@protonmail.com>
2021-11-18 04:44:17 +0000

    stable approve

---
+++ src/components/Bet/bettingmodal/bettingui.tsx
@@ -0,0 +1,162 @@
+import React, { useState, useCallback } from 'react'
+import useTransactionDeadline from '../../../hooks/useTransactionDeadline'
+import { AutoColumn } from '../../Column'
+import styled from 'styled-components'
+import { RowBetween } from '../../Row'
+import { TYPE, CloseIcon } from '../../../theme'
+import { ButtonConfirmed, ButtonError } from '../../Button'
+import ProgressCircles from '../../ProgressSteps'
+import CurrencyInputPanel from '../../CurrencyInputPanel'
+import { TokenAmount, Token } from 'elephantdexsdk'
+import { useActiveWeb3React } from '../../../hooks'
+import { maxAmountSpend } from '../../../utils/maxAmountSpend'
+import { useApproveCallback, ApprovalState } from '../../../hooks/useApproveCallback'
+import { useDerivedStakeInfo } from '../../../state/stake/hooks'
+//import { wrappedCurrencyAmount } from '../../utils/wrappedCurrency'
+import { TransactionResponse } from '@ethersproject/providers'
+import { useTransactionAdder } from '../../../state/transactions/hooks'
+import { usePitContract } from '../../../hooks/useContract'
+import { calculateGasMargin } from '../../../utils'
+import { PIT_SETTINGS } from '../../../constants'
+import useGovernanceToken from '../../../hooks/useGovernanceToken'
+
+/*const HypotheticalRewardRate = styled.div<{ dim: boolean }>`
+  display: flex;
+  justify-content: space-between;
+  padding-right: 20px;
+  padding-left: 20px;
+
+  opacity: ${({ dim }) => (dim ? 0.5 : 1)};
+`*/
+
+const ContentWrapper = styled(AutoColumn)`
+  width: 100%;
+  padding: 1rem;
+`
+
+interface StakingModalProps {
+  isOpen: boolean
+  onDismiss: () => void
+  stakingToken: Token
+  userLiquidityUnstaked: TokenAmount | undefined
+}
+
+export default function BettingUI({ isOpen, onDismiss, stakingToken, userLiquidityUnstaked }: StakingModalProps) {
+  const { chainId, library } = useActiveWeb3React()
+
+  // track and parse user input
+  const [typedValue, setTypedValue] = useState('')
+  const { parsedAmount, error } = useDerivedStakeInfo(typedValue, stakingToken, userLiquidityUnstaked)
+
+  const govToken = useGovernanceToken()
+  const pitSettings = chainId ? PIT_SETTINGS[chainId] : undefined
+
+  // state for pending and submitted txn views
+  const addTransaction = useTransactionAdder()
+  const [attempting, setAttempting] = useState<boolean>(false)
+  const [hash, setHash] = useState<string | undefined>()
+  const [failed, setFailed] = useState<boolean>(false)
+  const wrappedOnDismiss = useCallback(() => {
+    setHash(undefined)
+    setAttempting(false)
+    setFailed(false)
+    onDismiss()
+  }, [onDismiss])
+
+  const pit = usePitContract()
+
+  // approval data for stake
+  const deadline = useTransactionDeadline()
+  const [approval, approveCallback] = useApproveCallback(parsedAmount, pit?.address)
+
+  async function onStake() {
+    setAttempting(true)
+    if (pit && parsedAmount && deadline) {
+      if (approval === ApprovalState.APPROVED) {
+        const formattedAmount = `0x${parsedAmount.raw.toString(16)}`
+        const estimatedGas = await pit.estimateGas.enter(formattedAmount)
+
+        await pit
+          .enter(formattedAmount, {
+            gasLimit: calculateGasMargin(estimatedGas)
+          })
+          .then((response: TransactionResponse) => {
+            addTransaction(response, {
+              summary: `Deposit ${govToken?.symbol} to ${pitSettings?.name}`
+            })
+            setHash(response.hash)
+          })
+          .catch((error: any) => {
+            setAttempting(false)
+            if (error?.code === -32603) {
+              setFailed(true)
+            }
+            console.log(error)
+          })
+      } else {
+        setAttempting(false)
+        throw new Error('Attempting to stake without approval or a signature. Please contact support.')
+      }
+    }
+  }
+
+  // wrapped onUserInput to clear signatures
+  const onUserInput = useCallback((typedValue: string) => {
+    setTypedValue(typedValue)
+  }, [])
+
+  // used for max input button
+  const maxAmountInput = maxAmountSpend(userLiquidityUnstaked)
+  const atMaxAmount = Boolean(maxAmountInput && parsedAmount?.equalTo(maxAmountInput))
+  const handleMax = useCallback(() => {
+    maxAmountInput && onUserInput(maxAmountInput.toExact())
+  }, [maxAmountInput, onUserInput])
+
+  async function onAttemptToApprove() {
+    if (!pit || !library || !deadline) throw new Error('missing dependencies')
+    const liquidityAmount = parsedAmount
+    if (!liquidityAmount) throw new Error('missing liquidity amount')
+
+    return approveCallback()
+  }
+
+  return (
+    <ContentWrapper gap="lg">
+      {console.log(attempting, hash, failed)}
+      <RowBetween>
+        <TYPE.mediumHeader>Deposit</TYPE.mediumHeader>
+        <CloseIcon onClick={wrappedOnDismiss} />
+      </RowBetween>
+      <CurrencyInputPanel
+        value={typedValue}
+        onUserInput={onUserInput}
+        onMax={handleMax}
+        showMaxButton={!atMaxAmount}
+        currency={stakingToken}
+        label={''}
+        disableCurrencySelect={true}
+        customBalanceText={'Available to deposit: '}
+        id="stake-liquidity-token"
+      />
+
+      <RowBetween>
+        <ButtonConfirmed
+          mr="0.5rem"
+          onClick={onAttemptToApprove}
+          confirmed={approval === ApprovalState.APPROVED}
+          disabled={approval !== ApprovalState.NOT_APPROVED}
+        >
+          Approve
+        </ButtonConfirmed>
+        <ButtonError
+          disabled={!!error || approval !== ApprovalState.APPROVED}
+          error={!!error && !!parsedAmount}
+          onClick={onStake}
+        >
+          {error ?? 'Deposit'}
+        </ButtonError>
+      </RowBetween>
+      <ProgressCircles steps={[approval === ApprovalState.APPROVED]} disabled={true} />
+    </ContentWrapper>
+  )
+}
