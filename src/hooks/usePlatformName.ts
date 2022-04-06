fc36147f134b89eebcee6d99035b527f26491488
elephantproject <elephantproject@protonmail.com>
2022-04-06 18:11:37 +0000

    redundancy pcasino

--- src/hooks/usePlatformName.ts
+++ src/hooks/usePlatformName.ts
@@ -11,14 +11,14 @@ export default function usePlatformName(): string {
   const blockchain = useBlockchain()
   switch (blockchain) {
     case Blockchain.BINANCE_SMART_CHAIN:
-      return 'Elephant Dex - Harmony Defi & Casino | elephant.ac'
+      return 'Elephant Swap - HRC20 Exchange | Harmony Yield Farming | Defi & Casino | elephant.ac'
     case Blockchain.HARMONY:
       return govprice
         ? '$' + govToken?.name + ' - $' + govprice?.toFixed(3) + ' Trade HRC20 Tokens On Elephant.ac'
-        : 'Elephant Dex - Harmony Defi & Casino | elephant.ac'
+        : 'Elephant Swap - HRC20 Exchange | Harmony Yield Farming | Defi & Casino | elephant.ac'
     case Blockchain.ETHEREUM:
-      return 'Elephant Dex - Harmony Defi & Casino | elephant.ac'
+      return 'Elephant Swap - HRC20 Exchange | Harmony Yield Farming | Defi & Casino | elephant.ac'
     default:
-      return 'Elephant Dex - Harmony Defi & Casino | elephant.ac'
+      return 'Elephant Swap - HRC20 Exchange | Harmony Yield Farming | Defi & Casino | elephant.ac'
   }
 }
